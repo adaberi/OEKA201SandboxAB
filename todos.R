703099c223f5427567c01d482bfc1605e46a6511
yourGitHubadaberi <adab.berisha@gmail.com>
2025-02-10 19:15:27 +0100

    # Added todos exercises

--- todos.R
+++ todos.R
@@ -1,5 +1,15 @@
 # Todos
 ## Todo 1
+numbers <- c(4,5,8,11)
+sum(numbers)
 ## Todo 2
+sqrt(c(4, 9))
 ## Todo 3
+rnorm(n=100, mean = 0, sd = 2)
 ## Todo 4
+now <- as.numeric(substring(paste(Sys.Date()),1,4))
+started <- 2020
+born <- 1995
+spent <- (now-started)/(now-born)*100
+round(spent, digits=4)
+
